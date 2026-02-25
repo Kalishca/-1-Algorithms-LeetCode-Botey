857906331f57435d09e70021f4c80dc903385ce6
Kalishca <124444758+Kalishca@users.noreply.github.com>
2026-02-25 17:57:47 +0500

    Time: 823 ms (5.00%) | Memory: 20 MB (23.52%) - LeetSync

---
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+
+        def max_heapify(A, i, heap_size):
+            l = 2 * i
+            r = 2 * i + 1
+            largest = i
+
+            if l <= heap_size and A[l] > A[largest]:
+                largest = l
+
+            if r <= heap_size and A[r] > A[largest]:
+                largest = r
+
+            if largest != i:
+                A[i], A[largest] = A[largest], A[i]
+                max_heapify(A, largest, heap_size)
+
+        n = len(nums)
+        A = [0] + nums  # делаем 1-индексацию
+        heap_size = n
+
+        # build heap
+        for i in range(n // 2, 0, -1):
+            max_heapify(A, i, heap_size)
+
+        # извлекаем максимум k-1 раз
+        for _ in range(k - 1):
+            A[1], A[heap_size] = A[heap_size], A[1]
+            heap_size -= 1
+            max_heapify(A, 1, heap_size)
+
+        return A[1]
+        
