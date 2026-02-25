664ac3cdc6f5058164da7a2e1c2af96f18c77032
Kalishca <124444758+Kalishca@users.noreply.github.com>
2026-02-25 19:54:58 +0500

    Time: 788 ms (5.00%) | Memory: 20 MB (22.04%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -17,14 +17,12 @@ class Solution(object):
                 max_heapify(A, largest, heap_size)
 
         n = len(nums)
-        A = [0] + nums  # делаем 1-индексацию
+        A = [0] + nums
         heap_size = n
 
-        # build heap
         for i in range(n // 2, 0, -1):
             max_heapify(A, i, heap_size)
 
-        # извлекаем максимум k-1 раз
         for _ in range(k - 1):
             A[1], A[heap_size] = A[heap_size], A[1]
             heap_size -= 1
